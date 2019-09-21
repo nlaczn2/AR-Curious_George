6107840bc7a72c89dbd69d87d11662be07896376
Nicole Laczny <nicolelaczny@gmail.com>
2019-09-21 13:16:00 -0500

    back cover and new front cover

---
+++ AR - Curious George/Assets/speek2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class speek2 : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject button;
+    public GameObject person;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GameObject.Find("lacy_button2");
+        button.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        //cubeAni.GetComponent<Animator>();
+
+        person = GameObject.Find("cop");
+        person.GetComponent<GameObject>();
+        person.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        person.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        person.SetActive(false);
+    }
+
+
+}
